fcb11fd836c19348568ca6cdfdbe30046a354a57
Alexander Simpson <as2388@cam.ac.uk>
2018-05-25 12:15:09 +0100

    Pass in arguments via command line.

--- fetch_runs.py
+++ fetch_runs.py
@@ -1,3 +1,4 @@
+import argparse
 import time
 
 import requests
@@ -5,16 +6,24 @@ import requests
 if __name__ == "__main__":
     BASE_URL = "https://www.echomobile.org/api/cms/"
 
-    USER = ""
-    PASSWORD = ""
-    SURVEY = ""
+    parser = argparse.ArgumentParser(description="Poll EchoMobile for survey results")
+    parser.add_argument("echo_mobile_username", metavar="echo-mobile-username", help="", nargs=1)
+    parser.add_argument("echo_mobile_password", metavar="echo-mobile-password", help="User launching this program",
+                        nargs=1)
+    parser.add_argument("user", help="Identify of user launching this program", nargs=1)
+    parser.add_argument("survey", help="Name of survey to download results of", nargs=1)
 
-    AUTH = (USER, PASSWORD)
+    args = parser.parse_args()
+    echo_mobile_username = args.echo_mobile_username[0]
+    echo_mobile_password = args.echo_mobile_password[0]
+    target_survey_name = args.survey[0]
+
+    auth = (echo_mobile_username, echo_mobile_password)
 
     # Determine ID of survey
-    target_request = requests.get(BASE_URL + "survey", auth=AUTH)
+    target_request = requests.get(BASE_URL + "survey", auth=auth)
     target_response = target_request.json()
-    matching_surveys = [survey for survey in target_response["surveys"] if survey["name"] == SURVEY]
+    matching_surveys = [survey for survey in target_response["surveys"] if survey["name"] == target_survey_name]
 
     if len(matching_surveys) == 0:
         print("Error: Survey not found")
@@ -24,17 +33,19 @@ if __name__ == "__main__":
         print("Error: Survey name ambiguous")  # Not sure if this can actually happen but being defensive just in case.
         exit(1)
 
-    target = matching_surveys[0]["key"]
+    target_survey_id = matching_surveys[0]["key"]
 
     # Generate a report for that survey
-    generate_request = requests.post(BASE_URL + "report/generate", auth=AUTH,
-                                     params={"type": 13, "gen": "raw,label", "std_field": "", "target": target})
+    generate_request = requests.post(BASE_URL + "report/generate", auth=auth,
+                                     params={"type": 13, "gen": "raw,label", "target": target_survey_id})
     rkey = generate_request.json()["rkey"]
 
-    # Download the generated report, polling while it is unavailable
+    print(rkey)
+
+    # Download the generated report, polling until it becomes available
     serve_response = "Unauthorized"
     while serve_response == "Unauthorized":  # Server returns unauthorized until the report is ready.
-        serve_request = requests.get(BASE_URL + "serve", auth=AUTH, params={"rkey": rkey})
+        serve_request = requests.get(BASE_URL + "report/serve", auth=auth, params={"rkey": rkey})
         serve_response = serve_request.text
         time.sleep(2)
 
@@ -42,4 +53,4 @@ if __name__ == "__main__":
     print(serve_response)
 
     # Delete the background task we made when generating the report
-    cancel_request = requests.post(BASE_URL + "backgroundtask/cancel", auth=AUTH, params={"key": "report_" + rkey})
+    cancel_request = requests.post(BASE_URL + "backgroundtask/cancel", auth=auth, params={"key": "report_" + rkey})
