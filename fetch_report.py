4aa1e1ce39b3420a64a5ce3319c6406c0a89e1e8
Alexander Simpson <as2388@cam.ac.uk>
2018-06-04 12:49:10 +0100

    Update to use v0.2.0 of CoreDataModules

--- fetch_report.py
+++ fetch_report.py
@@ -1,12 +1,10 @@
 import argparse
-import json
 import os
 import time
-from io import BytesIO
+from io import StringIO
 
-import jsonpickle
 import requests
-from core_data_modules.traced_data.io import TracedDataCSVIO
+from core_data_modules.traced_data.io import TracedDataCSVIO, TracedDataJsonIO
 
 if __name__ == "__main__":
     BASE_URL = "https://www.echomobile.org/api/cms/"
@@ -50,13 +48,12 @@ if __name__ == "__main__":
     rkey = report_generate_request.json()["rkey"]
 
     # Poll for report status until the report has stopped generating.
-    # Status is not documented, but from observation 1 means generating and 3 means successfully generated s
+    # Status is not documented, but from observation '1' means generating and '3' means successfully generated
     report_status = 1
     while report_status == 1:
         report_status_request = requests.get(BASE_URL + "backgroundtask", auth=auth)
         report_status = report_status_request.json()["tasks"]["report_" + rkey]["status"]
         time.sleep(2)
-
     assert report_status == 3, "Report stopped generating, but with an unknown status"
 
     # Download the generated report
@@ -64,21 +61,13 @@ if __name__ == "__main__":
     report_serve_response = report_serve_request.text
 
     # Parse the downloaded report into a list of TracedData objects
-    data = list(TracedDataCSVIO.import_csv_to_traced_data_iterable(user, BytesIO(report_serve_response.encode())))
+    data = list(TracedDataCSVIO.import_csv_to_traced_data_iterable(user, StringIO(report_serve_response)))
 
     # Write the parsed items to a json file
     if os.path.dirname(output_path) is not "" and not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path))
-
-    with open(output_path, "wb") as f:
-        # Serialize the list of TracedData to a format which can be trivially deserialized.
-        pickled = jsonpickle.dumps(data)
-
-        # Pretty-print the serialized json
-        pp = json.dumps(json.loads(pickled), indent=2, sort_keys=True).encode()
-
-        # Write pretty-printed JSON to a file.
-        f.write(pp)
+    with open(output_path, "w") as f:
+        TracedDataJsonIO.export_traced_data_iterable_to_json(data, f, pretty_print=True)
 
     # Delete the background task we made when generating the report
     cancel_request = requests.post(BASE_URL + "backgroundtask/cancel", auth=auth, params={"key": "report_" + rkey})
