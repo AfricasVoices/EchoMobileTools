4624cdbc9536ccf57cea7bf1d1764b93750c3e3c
Alexander Simpson <as2388@cam.ac.uk>
2018-06-12 12:58:17 +0100

    Python 2 compatibility + doc updates

--- echo_mobile_session.py
+++ echo_mobile_session.py
@@ -1,22 +1,34 @@
 import time
 
+import six
 import requests
 
 
 class EchoMobileError(Exception):
+    """
+    Raised when the Echo Mobile server responded to a request with an error.
+    """
     pass
 
 
 class EchoMobileSession(object):
     """
+    A client-side API for interacting with Echo Mobile servers.
+    
+    For example, to download a survey report:
     >>> session = EchoMobileSession()
-    >>> session.login(USERNAME, PASSWORD)
-    >>> session.use_account_with_name(ACCOUNT_NAME)
-    >>> report = session.report_for_survey_name(SURVEY_NAME)
+    >>> session.login(<USERNAME>, <PASSWORD>)
+    >>> session.use_account_with_name(<ACCOUNT_NAME>)  # Optional for users with only one account
+    >>> report = session.report_for_survey_name(<SURVEY_NAME>)
+    >>> session.delete_session_background_tasks()  # Removes the report background task from the Echo Mobile website.
     """
     BASE_URL = "https://www.echomobile.org/api/"
 
     def __init__(self, verbose=False):
+        """
+        :param verbose: Whether to initialise with verbose mode enabled.
+        :type verbose: bool
+        """
         self.session = requests.Session()
         self.verbose = verbose
         self.background_tasks = set()
@@ -31,7 +43,7 @@ class EchoMobileSession(object):
         :type password: str
         """
         if self.verbose:
-            print("Logging in as '{}'... ".format(username), end="", flush=True)
+            six.print_("Logging in as '{}'... ".format(username), end="", flush=True)
 
         request = self.session.post(self.BASE_URL + "authenticate/simple",
                                     params={"_login": username, "_pw": password,
@@ -53,7 +65,7 @@ class EchoMobileSession(object):
         :rtype: list
         """
         if self.verbose:
-            print("Fetching user's accounts... ", end="", flush=True)
+            six.print_("Fetching user's accounts... ", end="", flush=True)
 
         request = self.session.get(self.BASE_URL + "cms/account/me", params={"with_linked": 1})
         response = request.json()
@@ -97,7 +109,7 @@ class EchoMobileSession(object):
         :type account_key: str
         """
         if self.verbose:
-            print("Switching to account '{}'... ".format(account_key), end="", flush=True)
+            six.print_("Switching to account '{}'... ".format(account_key), end="", flush=True)
 
         request = self.session.post(self.BASE_URL + "authenticate/linked", params={"acckey": account_key})
         response = request.json()
@@ -111,6 +123,8 @@ class EchoMobileSession(object):
         """
         Updates this session to use the account that has the given name.
 
+        Equivalent to choosing an account from the User -> Switch Accounts page of Echo Mobile.
+
         :param account_name: Name of account to switch to.
         :type account_name: str
         """
@@ -118,10 +132,10 @@ class EchoMobileSession(object):
 
     def surveys(self):
         """
-        Returns the list of surveys available to the logged in user/account.
+        Returns the list of active surveys available to the logged in user/account.
         """
         if self.verbose:
-            print("Fetching available surveys... ", end="", flush=True)
+            six.print_("Fetching available surveys... ", end="", flush=True)
 
         request = self.session.get(self.BASE_URL + "cms/survey")
         response = request.json()
@@ -160,18 +174,18 @@ class EchoMobileSession(object):
             print("Key for survey '{}' is '{}'".format(survey_name, survey_key))
 
         return survey_key
-    
+
     def await_report_generated(self, report_key, poll_interval=2):
         """
         Polls Echo Mobile for the status of a report until it is marked as complete.
-        
+
         :param report_key: Key of report to poll status of
         :type report_key: str
         :param poll_interval: Time to wait between polling attempts, in seconds.
         :type poll_interval: float
         """
         if self.verbose:
-            print("Waiting for report to generate... ", end="", flush=True)
+            six.print_("Waiting for report to generate... ", end="", flush=True)
 
         # Status is not documented, but from observation '1' means generating and '3' means successfully generated
         report_status = 1
@@ -188,7 +202,7 @@ class EchoMobileSession(object):
 
             if self.verbose and task["total"] != 0:
                 progress = task["progress"] / task["total"] * 100
-                print("\rWaiting for report to generate... {0:.2f}%".format(progress), end="", flush=True)
+                six.print_("\rWaiting for report to generate... {0:.2f}%".format(progress), end="", flush=True)
 
             report_status = task["status"]
 
@@ -224,14 +238,14 @@ class EchoMobileSession(object):
             contact_fields = ["name", "phone"]
 
         if self.verbose:
-            print("Requesting generation of report for survey '{}'... ".format(survey_key), end="", flush=True)
+            six.print_("Requesting generation of report for survey '{}'... ".format(survey_key), end="", flush=True)
 
         request = self.session.post(self.BASE_URL + "cms/report/generate",
                                     # Type is undocumented, but from inspection of the calls the website is
                                     # making it turns out that '13' is the magic number we need here.
                                     params={"type": 13, "target": survey_key,
-                                            "response_formats": ",".join(response_formats),
-                                            "contact_fields": ",".join(contact_fields)
+                                            "gen": ",".join(response_formats),
+                                            "std_field": ",".join(contact_fields)
                                             }
                                     )
         response = request.json()
@@ -252,7 +266,7 @@ class EchoMobileSession(object):
     def download_report(self, report_key):
         """
         Downloads the specified report from Echo Mobile.
-        
+
         Note that reports must first be generated before they can be downloaded.
 
         :param report_key: Key of report to download
@@ -261,7 +275,7 @@ class EchoMobileSession(object):
         :rtype: str
         """
         if self.verbose:
-            print("Downloading report... ", end="", flush=True)
+            six.print_("Downloading report... ", end="", flush=True)
 
         request = self.session.get(self.BASE_URL + "cms/report/serve", params={"rkey": report_key})
         response = request.text
@@ -292,16 +306,25 @@ class EchoMobileSession(object):
                                                  response_formats=response_formats, wait_until_generated=True)
         return self.download_report(report_key)
 
-    def report_for_survey_name(self, survey_name, **kwargs):
+    def report_for_survey_name(self, survey_name, contact_fields=None, response_formats=None):
         """
         Generates and downloads a report for the survey with the given name.
-        
-        :param survey_name: Name of survey to generate and download report for 
+
+        :param survey_name: Name of survey to generate and download report for
         :type survey_name: str
+        :param response_formats: List of response formats to download. Defaults to ["raw", "label"] if None.
+                                 The full list of options is : raw, label, value, score.
+        :type response_formats: list of str
+        :param contact_fields: List of contact fields to download. Defaults to ["name", "phone"] if None.
+                               The full list of options is: name, phone, internal_id, group, referrer, referrer_phone,
+                               upload_date, last_survey_complete_date, geo, locationTextRaw, labels, linked_entity,
+                               opted_out.
+        :type contact_fields: list of str
         :return: CSV containing the survey report
         :rtype: str
         """
-        return self.report_for_survey_key(self.survey_key_for_name(survey_name), kwargs)
+        return self.report_for_survey_key(self.survey_key_for_name(survey_name), contact_fields=contact_fields,
+                                          response_formats=response_formats)
 
     def delete_background_task(self, task_key):
         """
@@ -311,7 +334,7 @@ class EchoMobileSession(object):
         :type task_key: str
         """
         if self.verbose:
-            print("Deleting background task '{}'... ".format(task_key), end="", flush=True)
+            six.print_("Deleting background task '{}'... ".format(task_key), end="", flush=True)
 
         request = self.session.post(self.BASE_URL + "cms/backgroundtask/cancel", params={"key": task_key})
         response = request.json()
